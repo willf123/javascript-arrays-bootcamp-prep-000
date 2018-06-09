a215a09dfc08504e0dd28f3065da88703b2fe1da
willf123@yahoo.com <willf123@yahoo.com>
2018-06-09 20:11:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
   ];
   
   function addElementToBeginningOfArray(array, element) {
-    chocolateBars.push("twizzlers");
+    chocolateBars.unshift("twizzlers");
     return array;
   }
   
